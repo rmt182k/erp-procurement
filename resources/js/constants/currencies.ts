2bc15379cfe02366b6a9b9845ee35caa7c51c4e3
rmt182k <roytamba@outlook.com>
2026-02-12 01:40:49 +0700

    Create currencies.ts

---
+++ resources/js/constants/currencies.ts
@@ -0,0 +1,14 @@
+export const ISO_CURRENCIES = [
+    { code: 'IDR', name: 'Indonesian Rupiah', symbol: 'Rp' },
+    { code: 'USD', name: 'US Dollar', symbol: '$' },
+    { code: 'SGD', name: 'Singapore Dollar', symbol: 'S$' },
+    { code: 'EUR', name: 'Euro', symbol: '€' },
+    { code: 'JPY', name: 'Japanese Yen', symbol: '¥' },
+    { code: 'GBP', name: 'British Pound Sterling', symbol: '£' },
+    { code: 'AUD', name: 'Australian Dollar', symbol: 'A$' },
+    { code: 'CNY', name: 'Chinese Yuan', symbol: '¥' },
+    { code: 'HKD', name: 'Hong Kong Dollar', symbol: 'HK$' },
+    { code: 'KRW', name: 'South Korean Won', symbol: '₩' },
+    { code: 'MYR', name: 'Malaysian Ringgit', symbol: 'RM' },
+    { code: 'THB', name: 'Thai Baht', symbol: '฿' },
+];
